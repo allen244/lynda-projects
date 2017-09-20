9fde8b5ff8ba8c05a8464612ab6c7717d21331fc
asmaldone <alsmaldone@yahoo.com>
2017-09-20 12:57:46 -0400

    jf projects

--- java/jf/Exercise Files/02_01/TravelCost/src/travelcost/TravelCost.java
+++ java/jf/Exercise Files/02_01/TravelCost/src/travelcost/TravelCost.java
@@ -5,17 +5,29 @@
  */
 package travelcost;
 
-/**
- *
- * @author Peggy Fisher
- */
+import java.util.Scanner;
+
 public class TravelCost {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	public static void main(String[] args) {
+		// double distance;
+		// double mpg;
+		// double pricePerGallon;
+		// double totalCost;
+		double distance, mpg, pricePerGallon, totalCost;
+		String vehicleType;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the total distance in miles: ");
+		distance = in.nextDouble();
+		System.out.println("Enter the mpg for the vehicle: ");
+		mpg = in.nextDouble();
+		System.out.println("Enter the price of one gallon of gas: ");
+		pricePerGallon = in.nextDouble();
+		System.out.println("VTYP: ");
+		vehicleType = in.next();
+		totalCost = (distance / mpg) * pricePerGallon;
+		System.out.printf("The trip is going to cost $%5.2f Since you are driving a " + vehicleType , totalCost);
+		System.out.println();
+	}
+
 }
--- java/jf/Exercise Files/04_01/RealEstateListings_Final/src/realestatelistings_final/RealEstateListings.java
+++ java/jf/Exercise Files/04_01/RealEstateListings_Final/src/realestatelistings_final/RealEstateListings.java
@@ -1,6 +1,6 @@
 /*
- * This program is setup to create instances of the House class 
- * for a real estate company.  
+ * This program is setup to create instances of the House class
+ * for a real estate company.
  */
 package realestatelistings_final;
 
@@ -14,7 +14,12 @@ public class RealEstateListings {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Property land = new Property(10000,"Land", 2.1);
+        Property estate = new Property(250000, "Estate", 1.5, 3, 4);
+        System.out.println(land.toString());
+        System.out.println(estate.toString());
+        estate.setAskingPrice(230000);
+        System.out.println(estate.toString());
+      }
+
 }
